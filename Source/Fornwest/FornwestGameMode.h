db559e9aa7abf045617e4ba314921df631b9fbdf
Tim Dillon <tim@itsdits.com>
2021-05-07 11:10:27 +0900

    Initial commit

---
+++ Source/Fornwest/FornwestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FornwestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFornwestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFornwestGameMode();
+};
+
+
+
